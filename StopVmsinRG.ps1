132945051e4f1020563b5f0a14577bd2009fbf18
Stefan Denninger <stefan.denninger@acp.at>
2016-08-03 17:08:44 +0200

    Updated StopVmsinRG.ps1 in Azure Automation.

--- StopVmsinRG.ps1
+++ StopVmsinRG.ps1
@@ -17,30 +17,45 @@ workflow StopVmsinRG
 	
     $VMs = Get-AzureRmResourceGroup | ? { $_.ResourceGroupName -like $ResourceGroupName } | Get-AzureRmVM 
 		
+	$Sendnotification = 0
     #Print out up to 10 of those VMs
     if (!$VMs) {
         Write-Output "No VMs were found in your subscription."
     } else {
 		Foreach -parallel ($VM in $VMs) {
-	        Write-Output "Stopping " + $VM.Name
-		    $VM | Stop-AzureRmVM -Force -ErrorAction SilentlyContinue
-	        Write-Output "Stopped " + $VM.Name
+			$status = Get-AzureRmVM -Status -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+			if ( ($status.Statuses | ? { $_.Code -eq 'PowerState/running' }).Count -gt 0) {
+    			# VM is running, so turn it off
+		        Write-Output "Stopping " + $VM.Name
+			    $VM | Stop-AzureRmVM -Force -ErrorAction SilentlyContinue
+		        Write-Output "Stopped " + $VM.Name
+				$WORKFLOW:Sendnotification = 1
+			} else {
+				# 				
+				Write-Output "was already Stopped! -> " + $VM.Name
+			}
 		}
     }
     Write-Output "DONE."
 
-	# TODO put the AuthKey in KeyVault
-	$pushKey = Get-AutomationVariable -Name 'PushALotAuthKey'
-	$Body = '{
-	    "AuthorizationToken": "'+$pushKey+'",
-	    "Title": "StopVMs '+$ResourceGroupName+'",
-	    "IsImportant": "True",
-	    "Body": "Stop VMs in ResourceGroupName matching '+$ResourceGroupName+'",
-		"Source": "AzureAutom StopVmsinRG",
-		"TimeToLive": "200"
-	}'
-	# do not send Important Messages on Weekends ;)
-	if ( ((get-date).DayOfWeek.value__ -eq 6) -or ((get-date).DayOfWeek.value__ -eq 7) ) {  $Body = $Body.Replace('"IsImportant": "True"', '"IsImportant": "False"') }
-	Invoke-RestMethod -Method Post -Uri https://pushalot.com/api/sendmessage -ContentType 'application/json' -Body $Body
-	Write-Output "Notification DONE."
+	if ($sendnotification -eq 1) {
+		# TODO put the AuthKey in KeyVault
+		$pushKey = Get-AutomationVariable -Name 'PushALotAuthKey'
+		$Body = '{
+	    	"AuthorizationToken": "'+$pushKey+'",
+	    	"Title": "StopVMs '+$ResourceGroupName+'",
+	    	"IsImportant": "True",
+	    	"Body": "Stop VMs in ResourceGroupName matching '+$ResourceGroupName+'",
+			"Source": "AzureAutom StopVmsinRG",
+			"TimeToLive": "200"
+		}'
+		# do not send Important Messages on Weekends ;)
+		if ( ((get-date).DayOfWeek.value__ -eq 6) -or ((get-date).DayOfWeek.value__ -eq 7) ) {  $Body = $Body.Replace('"IsImportant": "True"', '"IsImportant": "False"') }
+		Invoke-RestMethod -Method Post -Uri https://pushalot.com/api/sendmessage -ContentType 'application/json' -Body $Body
+		Write-Output "Notification DONE."
+	} else {
+		Write-Output "nothing to notify."
+	}
 }
+
+# eof
